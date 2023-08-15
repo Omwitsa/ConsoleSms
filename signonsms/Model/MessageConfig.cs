9ee3c9f2494713ec49bdd355aa8c8f97ad6d3e83
Omwitsa <omwitsawilson@yahoo.com>
2023-08-15 15:46:30 +0300

    Create message configs

---
+++ signonsms/Model/MessageConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace signonsms.Model
+{
+    public class MessageConfig
+    {
+        public string Username { get; set; }
+        public string ApiKey { get; set; }
+        public string Saccocode { get; set; }
+    }
+}
--- signonsms/Program.cs
+++ signonsms/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using signonsms.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace signonsms
 {
@@ -6,46 +9,70 @@ namespace signonsms
     {
         static void Main(string[] args)
         {
-            System.Data.SqlClient.SqlDataReader dr;
-            sqlconnectionclass rd = new sqlconnectionclass();
-            dr = rd.ReadDB("SELECT * FROM Messages WHERE MsgType = 'Outbox'");
-            if (dr.HasRows)
+            sqlconnectionclass sqlConnector = new sqlconnectionclass();
+            var query = "SELECT * FROM MessageConfigs WHERE CLOSED = 0";
+            SqlDataReader configReader = sqlConnector.ReadDB(query);
+            var configs = new List<MessageConfig>();
+            if(configReader.HasRows)
             {
-                while (dr.Read())
+                while(configReader.Read())
                 {
-                    string username = "AMTECH_TECH";
-                    string apiKey = "752ae8eed0ea2e06bd5c56a75d5c2ab3e9c5f86ab590fc27b6e1059c6e156665";
+                    string username = configReader["username"].ToString();
+                    string apiKey = configReader["apiKey"].ToString();
+                    string saccocode = configReader["saccocode"].ToString();
 
-                    //string username = "Campus360dev";
-                    //string apiKey = "6645c5ec3750b1cd435459e36073c95c736586ed7b7ccdcf18c4cfd5cc5ebc9c";
-
-                    string msgid = dr["ID"].ToString().Trim();
-                    string telNo = dr["telephone"].ToString();
-                    string recipients = "+" + telNo;
-                    string message = dr["content"].ToString();
-
-                    AfricasTalkingGateway gateway = new AfricasTalkingGateway(username, apiKey);
-                    try
+                    configs.Add(new MessageConfig
                     {
+                        Username = username,
+                        ApiKey = apiKey,
+                        Saccocode = saccocode,
+                    });
+                }
 
-                        dynamic results = gateway.sendMessage(recipients, message, username);
+                configReader.Close(); configReader.Dispose(); configReader = null;
+            }
 
-                        foreach (dynamic result in results)
+            foreach(var config in configs)
+            {
+                query = "SELECT * FROM Messages WHERE MsgType = 'Outbox' AND Code='" + config.Saccocode + "'";
+                sqlConnector = new sqlconnectionclass();
+                SqlDataReader messageReader = sqlConnector.ReadDB(query);
+                if (messageReader.HasRows)
+                {
+                    while (messageReader.Read())
+                    {
+                        //string username = "AMTECH_TECH";
+                        //string apiKey = "752ae8eed0ea2e06bd5c56a75d5c2ab3e9c5f86ab590fc27b6e1059c6e156665";
+
+                        //string username = "Campus360dev";
+                        //string apiKey = "6645c5ec3750b1cd435459e36073c95c736586ed7b7ccdcf18c4cfd5cc5ebc9c";
+
+                        string msgid = messageReader["ID"].ToString().Trim();
+                        string telNo = messageReader["telephone"].ToString();
+                        string recipients = "+" + telNo;
+                        string message = messageReader["content"].ToString();
+
+                        AfricasTalkingGateway gateway = new AfricasTalkingGateway(config.Username, config.ApiKey);
+                        try
                         {
-                            if ((string)result["status"] == "Success")
+                            dynamic results = gateway.sendMessage(recipients, message, config.Username);
+                            foreach (dynamic result in results)
                             {
-                                string updatestatus = "update Messages set msgtype='Inbox' where ID='" + msgid + "'";
-                                new sqlconnectionclass().WriteDB(updatestatus);
-                                Console.WriteLine("message Send to '" + recipients + "'");
+                                if ((string)result["status"] == "Success")
+                                {
+                                    string updatestatus = "update Messages set msgtype='Inbox' where ID='" + msgid + "'";
+                                    new sqlconnectionclass().WriteDB(updatestatus);
+                                    Console.WriteLine("message Send to '" + recipients + "'");
+                                }
                             }
                         }
+                        catch (AfricasTalkingGatewayException e)
+                        {
+                            //
+                        }
                     }
-                    catch (AfricasTalkingGatewayException e)
-                    {
-                        //
-                    }
+                    messageReader.Close(); messageReader.Dispose(); messageReader = null;
                 }
-                dr.Close(); dr.Dispose(); dr = null;
             }
         }
     }
