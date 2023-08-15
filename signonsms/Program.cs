3ae89d58f5e4672281c11ab5020e18cb4ad95f25
Omwitsa <omwitsawilson@yahoo.com>
2023-08-15 10:26:09 +0300

    Save changes

--- signonsms/Program.cs
+++ signonsms/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace signonsms
 {
@@ -19,21 +16,19 @@ namespace signonsms
                     string username = "AMTECH_TECH";
                     string apiKey = "752ae8eed0ea2e06bd5c56a75d5c2ab3e9c5f86ab590fc27b6e1059c6e156665";
 
+                    //string username = "Campus360dev";
+                    //string apiKey = "6645c5ec3750b1cd435459e36073c95c736586ed7b7ccdcf18c4cfd5cc5ebc9c";
+
                     string msgid = dr["ID"].ToString().Trim();
-                    string phon = dr["telephone"].ToString();
-                   // string recipients = "+254" + dr["Telephone"].ToString().Substring(1);
-                    string recipients = "+" + dr["Telephone"].ToString();
-
+                    string telNo = dr["telephone"].ToString();
+                    string recipients = "+" + telNo;
                     string message = dr["content"].ToString();
-
-                    // Specify your AfricasTalking shortCode or sender id
-                    string from = "AMTECH_TECH";
 
                     AfricasTalkingGateway gateway = new AfricasTalkingGateway(username, apiKey);
                     try
                     {
 
-                        dynamic results = gateway.sendMessage(recipients, message, from);
+                        dynamic results = gateway.sendMessage(recipients, message, username);
 
                         foreach (dynamic result in results)
                         {
@@ -41,11 +36,11 @@ namespace signonsms
                             {
                                 string updatestatus = "update Messages set msgtype='Inbox' where ID='" + msgid + "'";
                                 new sqlconnectionclass().WriteDB(updatestatus);
-                                Console.WriteLine("message Send to '"+ recipients +"'");
+                                Console.WriteLine("message Send to '" + recipients + "'");
                             }
                         }
                     }
-                    catch (AfricasTalkingGatewayException)
+                    catch (AfricasTalkingGatewayException e)
                     {
                         //
                     }
