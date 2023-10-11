9d30a0cd316a1f2a5de11940564b46e9453b0dbb
Omwitsa <omwitsawilson@yahoo.com>
2023-10-11 13:18:49 +0300

    Rename Project

---
+++ POsSMS/Model/MessageConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POsSMS.Model
+{
+    public class MessageConfig
+    {
+        public string Username { get; set; }
+        public string ApiKey { get; set; }
+        public string SenderId { get; set; }
+        public string Saccocode { get; set; }
+    }
+}
